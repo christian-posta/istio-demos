0d7e4d504212cb10d3b5b985e2eacc83afb57c3b
Christian Posta <christian.posta@gmail.com>
2020-10-22 16:29:52 -0700

    removed old java demo

--- resilience/clean-istio-rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# for v1alpha3
-kubectl delete virtualservices.networking.istio.io  recommendation -n istio-demo
-kubectl delete virtualservices.networking.istio.io  preference -n istio-demo
-kubectl delete destinationrules.networking.istio.io  --all -n istio-demo
