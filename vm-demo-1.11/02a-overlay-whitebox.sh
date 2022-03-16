cd007a1d4748e2336ed1261c0c83f87fe6321677
Christian Posta <christian.posta@gmail.com>
2022-03-15 19:20:07 -0700

    remove root user for startup

--- vm-demo-1.11/02a-overlay-whitebox.sh
+++ vm-demo-1.11/02a-overlay-whitebox.sh
@@ -4,4 +4,5 @@ source ./env.sh
 # for whitebox mode
 echo "ISTIO_CUSTOM_IP_TABLES=true" >> "${WORK_DIR}"/files/sidecar.env
 echo "ISTIO_META_INTERCEPTION_MODE=NONE" >> "${WORK_DIR}"/files/sidecar.env
+echo "EXEC_USER=ceposta" >> "${WORK_DIR}"/files/sidecar.env
  
